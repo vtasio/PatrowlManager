a9209419081a12dbe2b3e316861a3abbc132b3d2
vtasio <tasiopoulos.vasilis@gmail.com>
2021-03-16 16:28:42 +0200

    Update utils.py
    rule fixes

--- engines/utils.py
+++ engines/utils.py
@@ -587,11 +587,11 @@ def _import_findings(findings, scan, engine_name=None, engine_id=None, owner_id=
                 new_raw_finding.save()
 
                 # Evaluate alerting rules
-                # try:
-                #     new_raw_finding.evaluate_alert_rules(trigger='auto')
-                # except Exception as e:
-                #     Event.objects.create(message="{} Error in alerting".format(evt_prefix),
-                #         type="ERROR", severity="ERROR", scan=scan, description=str(e))
+                try:
+                    new_raw_finding.evaluate_alert_rules(trigger='auto')
+                except Exception as e:
+                    Event.objects.create(message="{} Error in alerting".format(evt_prefix),
+                        type="ERROR", severity="ERROR", scan=scan, description=str(e))
 
                 known_findings_list.append(new_raw_finding.hash)
             else:
@@ -633,11 +633,11 @@ def _import_findings(findings, scan, engine_name=None, engine_id=None, owner_id=
                     new_finding.scopes.add(scope.id)
                 new_finding.save()
 
-            # Evaluate alerting rules
-            try:
-                new_finding.evaluate_alert_rules(trigger='auto')
-            except Exception as e:
-                Event.objects.create(message="{} Error in alerting".format(evt_prefix),
+                # Evaluate alerting rules
+                try:
+                    new_finding.evaluate_alert_rules(trigger='auto')
+                except Exception as e:
+                    Event.objects.create(message="{} Error in alerting".format(evt_prefix),
                         type="ERROR", severity="ERROR", scan=scan, description=str(e))
 
             # Vtasio Add Tags
